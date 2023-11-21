0904ced20093ae6008224a9f326769c4a063abd2
Heri Setyawan <herisetyawan233@gmail.com>
2023-11-21 22:11:35 +0700

    feat: create card service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,12 @@
 import CardSource from '@/components/cards/card-source';
 import Navbar from '@/components/navbar';
 import React from 'react';
+import WebDesign from "@/assets/img/web.png"
+import Mobile from "@/assets/img/mobile.png"
+import Source from "@/assets/img/source.png"
+import CardService from '@/components/cards/card-service';
 
 export default function Home() {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 3
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
   return (
     <React.Fragment>
       <Navbar/>
@@ -40,14 +25,16 @@ export default function Home() {
          
         </section>
 
-        <section className='bg-[#c8e7df] w-full rounded-sm'>
+        <section className='bg-[#c8e7df] w-full rounded-sm mb-20'>
           <div className='w-[90%] sm:max-w-6xl mx-auto py-10'>
             <div className='text-center'>
               <h3 className='font-medium text-green-500 text-lg'>we shall never deny guest even the most ridiculous request.</h3>
               <h1 className='text-slate-600 font-bold text-3xl mt-2'>Our Service</h1>
             </div>
             <div className='mt-5 flex flex-wrap sm:justify-center sm:flex-row sm:gap-x-5 flex-col gap-y-5'>
-      
+              <CardService image={WebDesign} title="Web Developer" deskripsi='Build and Service menyediakan layanan pembuatan dan perbaikan website, kami menguasai php, js, go, html dan css. Framework yang kita pakai adalah laravel,next js, react js, express, tailwind, bootstrap, dll' />
+              <CardService image={Mobile} title="Android Developer" deskripsi='Build and Service menyediakan layanan pembuatan dan perbaikan aplikasi mobile kami menggunakan framework flutter dalam penyelesainnya' />
+              <CardService image={Source} title="Source Coding" deskripsi='Build And Service juga menjual berbagai source code yang dapat kalian pelajari atau sebagai bahan pembuatan project kalian.' />
             </div>
           </div>
         </section>
---
+++ src/components/cards/card-service/index.tsx
@@ -0,0 +1,24 @@
+import CardServiceProps from "@/data/interfaces/props/card-service";
+import Image from "next/image";
+import Link from "next/link";
+
+const CardService = ({ title, image, deskripsi }: CardServiceProps) => {
+  return (
+    <div className='bg-white p-10 rounded-xl w-80 flex flex-col gap-y-5 justify-center items-center shadow-md hover:shadow-xl duration-500'>
+      <div className="w-56 h-56 relative">
+        <Image src={image} alt="" layout={"responsive"} />
+      </div>
+      <div className="mt-5 text-center w-full">
+        <h1 className="font-bold text-2xl text-green-600">{ title }</h1>
+        <p className="mt-5 text-gray-700">
+          { deskripsi }
+        </p>
+      </div>
+      <div className="w-full flex flex-col">
+        <Link href={'/'} className="w-full text-center bg-green-600 text-white rounded-md py-2">More</Link>
+      </div>
+    </div>
+  )
+}
+
+export default CardService;
