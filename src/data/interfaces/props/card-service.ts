0c93ebb961d5e7a3aa4d8837be5c68eafcf107d6
perdanaph <danangputro9@gmail.com>
2023-11-24 23:30:36 +0700

    feat: service page && refactor: change font; delete navbar item; module image; rename interface-props

---
+++ src/data/interfaces/props/card-service.ts
@@ -0,0 +1,7 @@
+import { StaticImageData } from 'next/legacy/image';
+
+export default interface CardServiceProps {
+  title: string;
+  deskripsi: string;
+  image: StaticImageData;
+}
--- src/data/interfaces/props/card-team.ts
+++ src/data/interfaces/props/card-team.ts
@@ -1,4 +1,4 @@
-import { StaticImageData } from 'next/image';
+import { StaticImageData } from 'next/legacy/image';
 
 interface CardTeamProps {
   image: StaticImageData;
