69027ed36078e8bcca7df691ffe4de3935cbff3a
Heri Setyawan <herisetyawan233@gmail.com>
2023-11-25 00:09:01 +0700

    refactor: blur & sticky navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/legacy/image';
 import Logo from '@/assets/logo.png';
 export default function Navbar() {
   return (
-    <header className='w-full h-20'>
+    <header className='w-full h-20 sticky top-0 backdrop-blur-sm bg-white/90 z-40'>
       <nav className='w-[90%] sm:max-w-6xl mx-auto flex h-full items-center font-medium justify-between'>
         <Image src={Logo} objectFit={'cover'} height={48} width={68} alt='alt' />
         <div className='flex items-center'>
