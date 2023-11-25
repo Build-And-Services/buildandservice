e8f63fd8c3b70fe6ca69d85e51df4cdb71106987
rafikusdiarto <rafikusdiarto1707@gmail.com>
2023-11-25 15:23:22 +0700

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import WebDesign from "@/assets/img/web.png"
 import Mobile from "@/assets/img/mobile.png"
 import Source from "@/assets/img/source.png"
 import CardService from '@/components/cards/card-service';
+import Image from 'next/image';
+import Marquee from 'react-fast-marquee';
 
 export default function Home() {
 
@@ -70,6 +72,236 @@ export default function Home() {
           </div>
         </section>
 
+        <div className="mt-[180px]">
+          <div className="text-center">
+            <h3 className="font-medium text-green-500 text-lg">
+              Selected based on specific needs and scale of the project.
+            </h3>
+            <h1 className="text-slate-600 font-bold text-3xl mt-2">
+              Our Tech Stack
+            </h1>
+          </div>
+
+          <Marquee
+            pauseOnHover
+            direction="right"
+            className="mt-[30px] py-[40px]"
+          >
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/vue-logomark.svg"
+                  width={80}
+                  height={80}
+                  alt="vue"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Vue
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Front-End Development
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/laravel.svg"
+                  width={80}
+                  height={80}
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Laravel
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Back-End Development
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white over:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/python-logomark.svg"
+                  width={80}
+                  height={80}
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Python
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">Data Science</div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/flutter.svg"
+                  width={80}
+                  height={80}
+                  className="max-w-[70px] max-h-[70px]"
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Flutter
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Mobile Development
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/flutter.svg"
+                  width={80}
+                  height={80}
+                  className="max-w-[70px] max-h-[70px]"
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Flutter
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Mobile Development
+                  </div>
+                </div>
+              </a>
+            </div>
+          </Marquee>
+
+          <Marquee pauseOnHover className="mb-[150px] py-[40px]">
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/vue-logomark.svg"
+                  width={80}
+                  height={80}
+                  alt="vue"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Vue
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Front-End Development
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/laravel.svg"
+                  width={80}
+                  height={80}
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Laravel
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Back-End Development
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white over:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/python-logomark.svg"
+                  width={80}
+                  height={80}
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Python
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">Data Science</div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/flutter.svg"
+                  width={80}
+                  height={80}
+                  className="max-w-[70px] max-h-[70px]"
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Flutter
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Mobile Development
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="items-center mr-20 w-[350px]">
+              <a
+                href="#"
+                className="p-[20px] w-full bg-white shadow-md border-4 border-white hover:text-blue-500 hover:border-solid hover:border-4 hover:border-green-500  rounded-lg inline-flex items-center justify-center "
+              >
+                <Image
+                  src="https://buildwithangga.com/themes/front/images/logo/flutter.svg"
+                  width={80}
+                  height={80}
+                  className="max-w-[70px] max-h-[70px]"
+                  alt="laravel"
+                />
+                <div className="text-left rtl:text-right ml-5">
+                  <div className="-mt-1 font-sans text-gray-700 text-xl font-bold">
+                    Flutter
+                  </div>
+                  <div className="mb-1 text-xs text-gray-700">
+                    Mobile Development
+                  </div>
+                </div>
+              </a>
+            </div>
+          </Marquee>
+        </div>
         <div className=''>
           <div>
             <h3 className='font-medium text-green-500 text-lg'>Info lebih lanjut</h3>
