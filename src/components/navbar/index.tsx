e984ba564d112f9754f806de9bc01338eae88d93
Faisal Bahari <fbhr1112@gmail.com>
2023-11-27 21:48:50 +0700

    fix: active navlink

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link';
 import Image from 'next/legacy/image';
 import Logo from '@/assets/logo.png';
+import { usePathname } from 'next/navigation';
+
 export default function Navbar() {
+  const pathname = usePathname()
+
   return (
     <header className='w-full h-20 sticky top-0 backdrop-blur-sm bg-white/90 z-40'>
       <nav className='w-[90%] sm:max-w-6xl mx-auto flex h-full items-center font-medium justify-between'>
         <Image src={Logo} objectFit={'cover'} height={48} width={68} alt='alt' />
         <div className='flex items-center'>
           <div className='ml-20 space-x-20 hidden lg:block '>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/service'}>Service</Link>
-            <Link href={'/team'}>Our Teams</Link>
+            <Link href={'/'} className={`hover:text-green-800 ${pathname == '/' ? 'text-green-600' : ''}`}>Home</Link>
+            <Link href={'/service'} className={`hover:text-green-800 ${pathname == '/service' ? 'text-green-600' : ''}`}>Service</Link>
+            <Link href={'/team'} className={`hover:text-green-800 ${pathname == '/team' ? 'text-green-600' : ''}`}>Our Teams</Link>
           </div>
         </div>
         <div>
